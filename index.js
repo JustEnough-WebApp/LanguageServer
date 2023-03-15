26664385f760475a4173738b455390275a49232a
juliedosher <juliegdosher@lewisu.edu>
2023-03-15 12:17:02 -0500

    created initial files and ping function

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser').json();
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors({
+	origin: ""                          // TODO: set url of language app client
+}));
+
+
+app.get('/', (req, res) => {
+	res.type('text/html')
+	res.send('Language App Server')     // TODO: change to name of app
+});
+
+app.get('/api/ping', bodyParser, (req, res) => {
+	console.log("Ping recieved");
+	res.type("text/html");
+	res.send("Ping received!");
+  });
+
+
+// Custom 404 page
+app.use((req, res) => {
+	res.type('text/plain')
+	res.status(404)
+	res.send('Not Found')
+});
+
+// Custom 500 page
+app.use(function (error, req, res, next) {
+	console.error(err.message)
+	res.type('text/plain')
+	res.status(500)
+	res.send('Server Error')
+});
+
+
+//Start your server on a specified port
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+});
